881dbb00b049b52ac373ae5409a1346607a0b700
Edgarunam <etobon96@gmail.com>
2023-06-20 00:49:57 -0600

    App Terminada

--- app/src/main/java/com/avv/cardiologoapp/ui/cardioResult/ResultCardioFragment.kt
+++ app/src/main/java/com/avv/cardiologoapp/ui/cardioResult/ResultCardioFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.View
 import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
@@ -35,14 +36,36 @@ class ResultCardioFragment : Fragment(R.layout.fragment_result_cardio) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentResultCardioBinding.bind(view)
 
+        binding.btnActualizar.setOnClickListener {
+
+            cardioViewModel.fetchCardioValues().observe(viewLifecycleOwner, Observer { result ->
+                when(result){
+                    is Results.Loading -> {
+
+                    }
+                    is Results.Success -> {
+
+                        Log.d("LiveData", result.data.toString())
+
+                        binding.rvCardioValues.adapter = CardioAdapter(requireContext(),result.data)
+                        Toast.makeText(requireContext(), "Actualizado", Toast.LENGTH_SHORT).show()
+                    }
+                    is Results.Failure -> {
+                        Log.d("LiveData", result.exception.toString())
+                        Toast.makeText(requireContext(),result.exception.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+            })
+
+        }
         cardioViewModel.fetchCardioValues().observe(viewLifecycleOwner, Observer { result ->
             when(result){
                 is Results.Loading -> {
-
+                    binding.progressBar.isVisible = true
                 }
                 is Results.Success -> {
+                    binding.progressBar.isVisible = false
                     Log.d("LiveData", result.data.toString())
-                    Toast.makeText(requireContext(),result.data.toString(), Toast.LENGTH_SHORT).show()
                     binding.rvCardioValues.adapter = CardioAdapter(requireContext(),result.data)
                 }
                 is Results.Failure -> {
